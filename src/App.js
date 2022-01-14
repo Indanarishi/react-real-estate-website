32cc2462b4d4c35a41b30b5fc4ce4b59b7edd009
Jundi <indana6969@gmail.com>
2022-01-14 15:03:34 +0700

    add the navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// styles
+import { GlobalStyles } from "./globalStyles";
+
+// components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      test
+      <Router>
+        <GlobalStyles />
+        <Navbar />
+      </Router>
     </>
   );
 }
